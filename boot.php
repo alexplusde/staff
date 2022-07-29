d3355c752f895baf25add627a1a9d6838e984cf4
Alexander Walther <github.com@alexplus.de>
2022-07-29 13:21:47 +0200

    Update boot.php
    fix #3

--- boot.php
+++ boot.php
@@ -1,7 +1,8 @@
 <?php
 
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
-rex_yform_manager_dataset::setModelClass(
+    rex_yform_manager_dataset::setModelClass(
     'rex_staff',
     staff::class
-);
+    );
+}
