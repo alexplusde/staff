a86b987e0def85c63cc365e40963cc79af31a218
Alexander Walther <aw@alexplus.de>
2022-08-08 17:37:30 +0200

    use staff editor via outputfilter

--- boot.php
+++ boot.php
@@ -2,7 +2,15 @@
 
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_dataset::setModelClass(
-    'rex_staff',
-    staff::class
+        'rex_staff',
+        staff::class
     );
+}
+
+if (rex::isBackend() && rex_be_controller::getCurrentPage() == "staff/edit") {
+    rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
+        $suchmuster = 'class="###staff-settings-editor###"';
+        $ersetzen = rex_config::get("staff", "editor");
+        $ep->setSubject(str_replace($suchmuster, $ersetzen, $ep->getSubject()));
+    });
 }
