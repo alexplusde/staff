fb5e85222181c008d6ed8394b4ca1ad713dbb1ad
Alexander Walther <aw@alexplus.de>
2022-08-08 10:36:51 +0200

    add methods

--- lib/staff.php
+++ lib/staff.php
@@ -16,8 +16,53 @@ class staff extends \rex_yform_manager_dataset
         $replace["%tel-cell%"] = $staff->getPhoneMobile();
         $replace["%tel-home%"] = $staff->getPhoneHome();
         $replace["%tel-work%"] = $staff->getPhoneWork();
-        $replace["%email-home%"] = $staff->getMail();
-        $replace["%email-work%"] = $staff->getMail();
+        $replace["%email-home%"] = $staff->getMailHome();
+        $replace["%email-work%"] = $staff->getMailWork();
         return (new QRCode)->render(str_replace($find, $replace, $fragment));
+    }
+
+    public function getFirstName()
+    {
+        return $this->getValue('firstname');
+    }
+    public function getLastName()
+    {
+        return $this->getValue('firstname');
+    }
+    public function getTitle()
+    {
+        return $this->getValue('firstname');
+    }
+    public function getCompany()
+    {
+        return $this->getValue('company');
+    }
+    public function getEmailWork()
+    {
+        return $this->getValue('email-work');
+    }
+    public function getEmailHome()
+    {
+        return $this->getValue('email-work');
+    }
+    public function getStreet()
+    {
+        return $this->getValue('street');
+    }
+    public function getCountry()
+    {
+        return $this->getValue('country');
+    }
+    public function getTelCell()
+    {
+        return $this->getValue('cell_phone');
+    }
+    public function getTelWork()
+    {
+        return $this->getValue('telephone_work');
+    }
+    public function getTelHome()
+    {
+        return $this->getValue('telephone_work');
     }
 }
