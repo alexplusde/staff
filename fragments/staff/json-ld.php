735effa94bc3789e79222b5b9e7d00a6c37c596b
alxndr-w <alxndr-w@users.noreply.github.com>
2024-07-08 09:38:39 +0000

    Apply php-cs-fixer changes

--- fragments/staff/json-ld.php
+++ fragments/staff/json-ld.php
@@ -6,21 +6,21 @@
 $person = staff::get($this->getVar('id'));
 
 $jsonLd = [
-    "@context" => "https://schema.org",
-    "@type" => "Person",
-    "address" => [
-        "@type" => "PostalAddress",
-        "addressLocality" => $person->getCity(),
-        "addressRegion" => $person->getRegion(),
-        "postalCode" => $person->Zip(),
-        "streetAddress" => $person->getStreet()
+    '@context' => 'https://schema.org',
+    '@type' => 'Person',
+    'address' => [
+        '@type' => 'PostalAddress',
+        'addressLocality' => $person->getCity(),
+        'addressRegion' => $person->getRegion(),
+        'postalCode' => $person->Zip(),
+        'streetAddress' => $person->getStreet(),
     ],
-    "email" => "mailto:" . $person->getEmail(),
-    "image" => $person->getMediaUrl(),
-//  "jobTitle" => $person->getJobTitle(),
-    "name" => $person->getName() ?? $person->getFullname(),
-    "telephone" => $person->getPhone(),
-//  "url" => $person->getUrl()
+    'email' => 'mailto:' . $person->getEmail(),
+    'image' => $person->getMediaUrl(),
+    //  "jobTitle" => $person->getJobTitle(),
+    'name' => $person->getName() ?? $person->getFullname(),
+    'telephone' => $person->getPhone(),
+    //  "url" => $person->getUrl()
 ];
 
 echo '<script type="application/ld+json">' . json_encode($jsonLd, JSON_UNESCAPED_SLASHES) . '</script>';
